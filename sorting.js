0be851e450f6261bb0b217c42e59db1c912befb1
Shashank Yadav <shashankyadav724@gmail.com>
2025-11-22 20:01:22 +0530

    Sorting the Array

---
+++ sorting.js
@@ -0,0 +1,31 @@
+//Write a program to sort the array
+
+let arr = [8, -2, 10, 3, 1, 4, 9, 0];
+
+let temp;
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  for (let j = i + 1; j <= arr.length - 1; j++) {
+    if (arr[i] > arr[j]) {
+      temp = arr[i]; //Swaping using third varibale "temp"
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+}
+console.log(arr);
+ 
+//or
+
+let arr1 = [8, -2, 10, 3, 1, 4, 9, 0];
+
+for (let k = 0; k <= arr1.length - 1; k++) {
+  for (let l = k + 1; l <= arr1.length - 1; l++) {
+    if (arr1[k] > arr1[l]) {
+      arr1[k] = arr1[k] + arr1[l]; //Swaping without using Third variable
+      arr1[l] = arr1[k] - arr1[l];
+      arr1[k] = arr1[k] - arr1[l];
+    }
+  }
+}
+console.log(arr1);
